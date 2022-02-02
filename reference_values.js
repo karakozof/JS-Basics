3c56b3df80da41b5552862830943f8ec2981ef51
Ilya Karakozov <karakozof@gmail.com>
2022-02-02 18:56:18 +0200

    added reference_values.js

---
+++ reference_values.js
@@ -0,0 +1,12 @@
+const objectA = {
+    a: 10,
+    b: 20
+}
+
+const copyOfA = objectA
+
+copyOfA.a = 20 //Change the value of property 'a' of the object
+
+copyOfA.c = 'abc' //Add property 'c' with value 'abc' to the object
+
+objectA
