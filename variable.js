9c40fb38f692aa8d4d09724da0f992e8cb9bf96a
Ilya Karakozov <karakozof@gmail.com>
2022-02-02 18:55:16 +0200

    add practice to the variable.js

--- variable.js
+++ variable.js
@@ -1,8 +1,28 @@
 let myName
+
 console.log(myName)
+
 myName = 'Ilya'
+
 console.log(myName)
+
 console.log(console.log(10))
 
 const name = 'Ilya'
+
 console.log(myName)
+
+//reference values
+const objectA = {
+    a: 10,
+    b: 20
+}
+
+const copyOfA = objectA
+
+copyOfA.a = 20 //Change the value of property 'a' of the object
+
+copyOfA.c = 'abc' //Add property 'c' with value 'abc' to the object
+
+objectA
+
