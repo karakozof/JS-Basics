67280dbb6219eb092c624600cefa2ac11ac052e0
Ilya Karakozov <karakozof@gmail.com>
2022-02-09 18:24:02 +0200

    refactoring reference_values

--- reference_values.js
+++ reference_values.js
@@ -7,8 +7,8 @@ console.log(`a: ${objectA.a}  b: ${objectA.b}`)
 
 const copyOfA = objectA
 
-copyOfA.a = 20 //Change the value of property 'a' of the object
+copyOfA.a = 20 //Change the value of property 'a'
 
-copyOfA.c = 'abc' //Add property 'c' with value 'abc' to the object
+copyOfA.c = 'abc' //Add property 'c' with value 'abc'
 
-console.log(`a: ${copyOfA.a} b: ${copyOfA.b} c:${copyOfA.c}`)
+console.log(`a: ${copyOfA.a} b: ${copyOfA.b} c:${objectA.c}`)
