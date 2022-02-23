707f9a736ba2d6a41b2529c045839d238bd1c2cf
Ilya Karakozov <karakozof@gmail.com>
2022-02-23 18:38:08 +0200

    refactoring

--- And or/and_or.js
+++ And or/and_or.js
@@ -8,20 +8,22 @@ else {
 };
 
 let whoIs = prompt('Who is there?')
-let userPass
 
 if (whoIs === "Admin") {
-    userPass = prompt("Enter your pass")
+
+    let userPass = prompt("Enter your pass")
+
+    if (userPass === 'Main') {
+        alert(`Hello ${whoIs} !`)
+    } else if (userPass === null || userPass === '') {
+        alert('Canceled')
+    } else {
+        alert('Wrong password')
+    }
+
 } else if (whoIs === null || whoIs === '') {
     alert('Canceled')
 } else {
     alert(" i don't now you")
 }
 
-if (userPass === 'Main') {
-    alert(`Hello ${whoIs} !`)
-} else if (userPass === null || userPass === '') {
-    alert('Canceled')
-} else {
-    alert('Wrong password')
-}
--- While for/while_for.js
+++ While for/while_for.js
@@ -14,3 +14,14 @@ let number
 do {
     number = prompt('Enter a number greater than 100')
 } while (number <= 100 && number);
+
+let number = prompt("Enter the number")
+
+nextPrime:
+for (let i = 2; i <= number; i++) {
+
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) continue nextPrime;
+    }
+    alert(i);
+}
