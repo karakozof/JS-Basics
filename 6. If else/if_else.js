54a2641122ede6402145c2f9465f201d056aca92
Ilya Karakozov <karakozof@gmail.com>
2022-04-07 19:44:56 +0400

    added if_else.js

---
+++ 6. If else/if_else.js
@@ -0,0 +1,19 @@
+let question = prompt("What's the official name of JavaScript?");
+
+if (question === "ECMAScript") {
+    alert("You're right.")
+} else {
+    alert("You don't know? ECMAScript")
+};
+
+
+
+let whatIsTheNumber = prompt("Enter the number");
+
+if (whatIsTheNumber > 0) {
+    alert(1)
+} if (whatIsTheNumber < 0) {
+    alert(-1)
+} if (whatIsTheNumber === 0) {
+    alert(0)
+};
